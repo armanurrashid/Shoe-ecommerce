d68f4daa51af4a1a0f49b86b27ed75ed8fcea581
Armanur Rashid <58313733+Arman13-arch@users.noreply.github.com>
2023-10-11 10:02:19 +0600

    Value stored and retrieve from local storage

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,30 +3,32 @@ import './Cart.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
-const Cart = ({cart}) => {
-    console.log(cart);
-    // const {cart} = props 
 let total = 0;
+let total_shipping = 0;
+const Cart = ({cart}) => {
 for (const product of cart){
+    total_shipping = total_shipping + product.shipping;
     total = total + product.price;
 }
+let total_tax = (total*7)/100
+let total_grand = total + total_shipping + total_tax
     return (
         <div className='cart'>
             <h3>Order</h3>
+            <hr />
             <p>Selected item: {cart.length}</p>
-            <p>Total Price: {total}</p>
-            <p>Total Shipping: </p>
-            <p>Tax: </p>
-            <h4>Grand Total:  </h4>
+            <p>Total Price: ${total}</p>
+            <p>Total Shipping: ${total_shipping}</p>
+            <p>Tax: ${total_tax.toFixed(2)}</p>
+            <h4>Grand Total: ${total_grand.toFixed(2)}</h4>
             <button className='btn-clear-cart'><span>Clear Cart </span>
             <FontAwesomeIcon icon={faTrashAlt} />
             </button>
-            <button className='btn-review-cart'><span>Review Order </span>
+            <button className='btn-review-cart'><span>Review Order</span>
             <FontAwesomeIcon icon={faArrowRight} />
             </button>
-            
         </div>
     );
+    
 };
-
 export default Cart;
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -3,24 +3,44 @@ import Product from '../Product/Product';
 import './Shop.css'
 import { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
-
+import { addToDb, getShoppingCart } from "../../utilities/fakedb"
 const Shop = () => {
-    const [products,setProducts] = useState([]);
-    useEffect(()=>{
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
         fetch('products.json')
-        .then((res)=>res.json())
-        .then((data)=> setProducts(data))
-    },[])
+            .then((res) => res.json())
+            .then((data) => setProducts(data))
+    }, [])
+
+    useEffect(() => {
+        // console.log("products: ", products)
+        const storedCart = getShoppingCart();
+        const savedCart = [];
+        // console.log("Hello")
+        console.log(storedCart)
+        for (const id in storedCart) {
+            const addedProduct = products.find(product => product.id === id)
+            if (addedProduct) {
+                const quantity = storedCart[id]
+                addedProduct.quantity = quantity
+                savedCart.push(addedProduct)
+                // console.log(addedProduct)
+            }
+        }
+        setCart(savedCart);
+    }, [products])
+
     const [cart, setCart] = useState([])
     const handleAddToCart = (product) => {
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id)
     }
     return (
         <div className='shop-container'>
             <div className="products-container">
                 {
-                    products.map(product => <Product key={product.id} product = {product} handleAddToCart={handleAddToCart}></Product>)
+                    products.map(product => <Product key={product.id} product={product} handleAddToCart={handleAddToCart}></Product>)
                 }
             </div>
             <div className="cart-container">
